371b0a975a0f777ceb99d5bcc35457c3f5475ba6
Justine <98328323+JustineD22@users.noreply.github.com>
2022-03-04 10:34:14 +0100

    commit

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,22 +6,35 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController {
+class DefaultController
+{
     #[Route('/', name: 'index')]
     public function index()
     {
         return new Response('<h1>Je fais du Symfony</h1>');
     }
-    #[Route('/toto', name: 'toto')]
-    public function toto(Request $request)
+
+    #[Route('/toto/{name}', name: 'toto', methods: ['GET'], schemes: ['https'])]
+    public function toto(string $name = "truc")
     {
-        // $request = Request::createFromGlobals();
-        //dd($request);
-        $valeur = $request->query->get('valeur');
-        $response = new Response("<h1> La valeur est $valeur </h1>",
-        Response::HTTP_NOT_FOUND, 
-        ['content-type' => 'text/html']
-    );
+        $response = new Response(
+            "<h1>La valeur est $name</h1>",
+            Response::HTTP_NOT_FOUND,
+            ['content-type' => 'text/html']
+        );
+
         return $response;
+    }
+
+    #[Route(path: '/{id}', name: 'testById', priority: 1, defaults: ['title' => 'La page a Michel'] ,requirements: ['id' => '\d+'], methods: ['GET'], schemes: ['https'])]
+    public function testById(Request $request, int $id)
+    {
+        return new Response("<h1>La valeur est $id</h1>");
+    }
+
+    #[Route(path: '/{name}', name: 'testByName', priority: 2, methods: ['GET'], schemes: ['https'])]
+    public function testByName(string $name = "defaut")
+    {
+        return new Response("<h1>Le mot est $name</h1>");
     }
 }
