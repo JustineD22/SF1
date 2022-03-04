db327fcf9a81144ac3cd5ec9ccc3d7cbd42ec19d
Justine <98328323+JustineD22@users.noreply.github.com>
2022-03-04 16:52:33 +0100

    git commit

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -10,16 +10,14 @@ class PageController extends AbstractController
 {
     #[Route('/page', name: 'app_page')]
     public function index(): Response
-    {   
+    {
         $users = [
             ['username' => 'Michel'],
             ['username' => 'Bob'],
             ['username' => 'Jean-Patrick'],
             ['username' => 'Josianne']
         ];
-        return $this->render('base.html.twig', ['users' => $users]);
-
-        //return $this->file('fichier.txt');
+        return $this->render('base.html.twig');
         // return $this->json([
         //     'message' => 'Welcome to your new controller!',
         //     'path' => 'src/Controller/PageController.php',
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,13 +6,29 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route(path: '/user', name: 'user_')]
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
+    const users = [
+        ['id' => 1, 'name' => 'Michel'],
+        ['id' => 3, 'name' => 'René'],
+        ['id' => 7, 'name' => 'Gérard'],
+        ['id' => 22, 'name' => 'Raymond'],
+    ];
+
+    #[Route(path: '/', name: 'index')]
     public function index(): Response
     {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
-        ]);
+        return $this->render('/user/index.html.twig', ['users' => self::users]);
     }
+
+    #[Route(path: '/{id}', name: 'profile')]
+    public function profile(string $id): Response
+    {
+        $userIndex = array_search($id, array_column(self::users, 'id'));
+        if ($userIndex === false) {
+            throw $this->createNotFoundException("User does not exist!");
+        }
+        return $this->render('/user/index.html.twig', ['users' => [self::users[$userIndex]]]);
+    }    
 }
